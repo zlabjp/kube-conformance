add38593479687f507255e424e9d5f8ff30bf1f3
Kazuki Suda <ksuda@zlab.co.jp>
2020-03-26 15:44:50 +0900

    Bump the version to v1.18.0

--- hack/update-kubernetes-version.sh
+++ hack/update-kubernetes-version.sh
@@ -11,3 +11,4 @@ fi
 
 sed -i -e "s#kube-conformance:[0-9.]\+#kube-conformance:$kubernetes_version#" README.md
 sed -i -e "s/KUBERNETES_VERSION=[0-9\.]\+/KUBERNETES_VERSION=$kubernetes_version/" Makefile
+sed -i -e "s#image: kindest/node:v[0-9\.]\+#image: kindest/node:v${kubernetes_version}#" .github/workflows/main.yaml
